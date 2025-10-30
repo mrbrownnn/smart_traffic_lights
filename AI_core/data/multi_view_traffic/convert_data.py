4e4098e1dcef608f3372e960896e8236265ca4d7
mrbrownnn <thanhpham4655@gmail.com>
2025-10-30 16:43:55 +0700

    save current work

--- AI_core/data/multi_view_traffic/convert_data.py
+++ AI_core/data/multi_view_traffic/convert_data.py
@@ -35,24 +35,112 @@ def convert(json_path, img_root, out_root, valid_ids):
 
         converted += 1
 
-    print(f"✅ {json_path} done. Converted {converted}, skipped {skipped}.")
+    print(f" {json_path} done. Converted {converted}, skipped {skipped}.")
 
 
 # Mapping COCO ids -> YOLO ids
-valid_ids = {2:0, 3:1, 6:2, 8:3}
+valid_ids = {2:0, 3:1, 5:2, 7:3}
+
+
 
 # Convert drone
 convert(
-    json_path="E:/multi_view_traffic/drone-mscoco.json",
-    img_root="E:/multi_view_traffic/Drone",
-    out_root="E:/multi_view_traffic/converted_yolo/drone_labels",
+    json_path="C:/Users/admin/Desktop/project_local_backup/AI_core/data/multi_view_traffic/drone-mscoco.json",
+    img_root="C:/Users/admin/Desktop/project_local_backup/AI_core/data/multi_view_traffic/Drone",
+    out_root="C:/Users/admin/Desktop/project_local_backup/AI_core/data/multi_view_traffic/converted_yolo(2)/drone_labels",
     valid_ids=valid_ids
 )
 
 # Convert infrastructure
 convert(
-    json_path="E:/multi_view_traffic/infrastructure-mscoco.json",
-    img_root="E:/multi_view_traffic/Infrastructure",
-    out_root="E:/multi_view_traffic/converted_yolo/infrastructure_labels",
+    json_path="/Users/admin/Desktop/project_local_backup/AI_core/data/multi_view_traffic/infrastructure-mscoco.json",
+    img_root="C:/Users/admin/Desktop/project_local_backup/AI_core/data/multi_view_traffic/Infrastructure",
+    out_root="C:/Users/admin/Desktop/project_local_backup/AI_core/data/multi_view_traffic/converted_yolo(2)/infrastructure_labels",
     valid_ids=valid_ids
 )
+""" COCOS to YOLO format
+{
+  "class": {
+    "0": "person",
+    "1": "bicycle",
+    "2": "car",
+    "3": "motorcycle",
+    "4": "airplane",
+    "5": "bus",
+    "6": "train",
+    "7": "truck",
+    "8": "boat",
+    "9": "traffic light",
+    "10": "fire hydrant",
+    "11": "stop sign",
+    "12": "parking meter",
+    "13": "bench",
+    "14": "bird",
+    "15": "cat",
+    "16": "dog",
+    "17": "horse",
+    "18": "sheep",
+    "19": "cow",
+    "20": "elephant",
+    "21": "bear",
+    "22": "zebra",
+    "23": "giraffe",
+    "24": "backpack",
+    "25": "umbrella",
+    "26": "handbag",
+    "27": "tie",
+    "28": "suitcase",
+    "29": "frisbee",
+    "30": "skis",
+    "31": "snowboard",
+    "32": "sports ball",
+    "33": "kite",
+    "34": "baseball bat",
+    "35": "baseball glove",
+    "36": "skateboard",
+    "37": "surfboard",
+    "38": "tennis racket",
+    "39": "bottle",
+    "40": "wine glass",
+    "41": "cup",
+    "42": "fork",
+    "43": "knife",
+    "44": "spoon",
+    "45": "bowl",
+    "46": "banana",
+    "47": "apple",
+    "48": "sandwich",
+    "49": "orange",
+    "50": "brocolli",
+    "51": "carrot",
+    "52": "hot dog",
+    "53": "pizza",
+    "54": "donut",
+    "55": "cake",
+    "56": "chair",
+    "57": "couch",
+    "58": "potted plant",
+    "59": "bed",
+    "60": "dining table",
+    "61": "toilet",
+    "62": "tv",
+    "63": "laptop",
+    "64": "mouse",
+    "65": "remote",
+    "66": "keyboard",
+    "67": "cell phone",
+    "68": "microwave",
+    "69": "oven",
+    "70": "toaster",
+    "71": "sink",
+    "72": "refrigerator",
+    "73": "book",
+    "74": "clock",
+    "75": "vase",
+    "76": "scissors",
+    "77": "teddy bear",
+    "78": "hair drier",
+    "79": "toothbrush"
+  }
+}
+"""
